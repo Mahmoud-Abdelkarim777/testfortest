5ca200b58f9340a679b04049bc055edcca933ab4
Mahmoud-Abdelkarim777 <mabdelkarimkhalaf777@gmail.com>
2025-02-10 04:39:57 +0200

    first commit

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -14,13 +14,14 @@ export async function handler(event) {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     const response = await resend.emails.send({
-      from: 'mabdelkarimkhalaf777@gmail.com', // استخدم بريدًا مسجلاً في Resend//-
+      from: 'mydomain.mahmoud.com', // استخدم بريدًا مسجلاً في Resend//-
       to: 'elqanony777@gmail.com',
       subject: `رسالة جديدة من ${name}`,
       reply_to: email,
       text: `الاسم: ${name}\nالإيميل: ${email}\n\nالرسالة:\n${message}`,
     });
     
+console.log(response);
 
     return {
       statusCode: 200,
